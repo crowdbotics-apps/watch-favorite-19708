bbf5c01bea3f92b4d5ae8d74e8fc221dfb971f7e
Crowdbotics <team@crowdbotics.com>
2020-08-21 17:00:54 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging92854', human_name: 'Messaging', access_route: 'Messaging92854', icon: 'comments'},
 { name: 'ArticleList92853', human_name: 'Article List', access_route: 'ArticleList92853'},
 { name: 'Achievements92852', human_name: 'Achievements', access_route: 'Achievements92852'},
 { name: 'Add-Item92849', human_name: 'Add-item', access_route: 'Add-Item92849'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Messaging92854Navigator from '../features/Messaging92854/navigator';
 import ArticleList92853Navigator from '../features/ArticleList92853/navigator';
 import Achievements92852Navigator from '../features/Achievements92852/navigator';
 import Add-Item92849Navigator from '../features/Add-Item92849/navigator';
@@ -18,6 +19,7 @@ import Contacts92826Navigator from '../features/Contacts92826/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Messaging92854: { screen: Messaging92854Navigator },
 ArticleList92853: { screen: ArticleList92853Navigator },
 Achievements92852: { screen: Achievements92852Navigator },
 Add-Item92849: { screen: Add-Item92849Navigator },
