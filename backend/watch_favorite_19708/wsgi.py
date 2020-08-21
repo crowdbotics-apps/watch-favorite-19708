dedac1d39780d8c4aa643d9c5c5f71299ff9881a
Crowdbotics <team@crowdbotics.com>
2020-08-21 16:59:20 +0000

    Update models

--- backend/watch_favorite_19708/wsgi.py
+++ backend/watch_favorite_19708/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watch_favorite_19708.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watch_favorite_19708.settings")
 
 application = get_wsgi_application()
